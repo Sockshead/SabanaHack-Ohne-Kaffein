b6aa971d1bd55ec80819e990988e8aaa52f84627
Sockshead <juancapoar@gmail.com>
2019-08-04 11:46:19 -0500

    Create Modelo_Perfiles_ML.py

---
+++ Modelo_Perfiles_ML.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  4 09:06:31 2019
+
+@author: juanc
+"""
+
+print(__doc__)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import neighbors, datasets
+
+n_neighbors = 6
+
+iris = datasets.load_iris()
+X = iris.data[:, :2]
+y = iris.target
+
+h = .02
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+clf = neighbors.KNeighborsClassifier(n_neighbors)
+clf.fit(X, y)
+
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+Z = Z.reshape(xx.shape)
+plt.figure()
+plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+                edgecolor='k', s=20)
+
+plt.title("Afinidad Arte - Deportes")
+plt.xlabel("Arte")
+plt.ylabel("Deporte")
+
+
+plt.show()
